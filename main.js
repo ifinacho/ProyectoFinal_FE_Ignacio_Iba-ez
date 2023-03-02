848a5b202f21a4347bb1e2b818e7e4c0ee168e45
Ignacio Francisco Ibañez <endermindkai@gmail.com>
2023-03-01 21:37:45 -0300

    Primer commit creando el proyecto

---
+++ main.js
@@ -0,0 +1,35 @@
+/*let datosPersonales = document.getElementById("datosPersonales");
+let botonOcultar = document.getElementById("botonOcultar");
+let botonMostrar = document.getElementById("botonMostrar");
+
+
+
+
+botonOcultar.onclick = () => datosPersonales.style.display = "none";
+botonMostrar.onclick = () => datosPersonales.style.display = "";*/
+
+let divdatos = document.getElementsByClassName("divMain");
+let botonesOcultar = document.getElementsByClassName("botonOcultar");
+let botonesMostrar = document.getElementsByClassName("botonMostrar");
+
+
+for(let i = 0; i < botonesOcultar.length; i++) {
+    let botonOcultar = botonesOcultar[i];
+    for(let j = 0; j < divdatos.length; j++) {
+        let divDato = divdatos[j];
+        if(botonOcultar.classList[1] == divDato.classList[1]){
+            botonOcultar.onclick = () => divDato.style.display = "none";
+        }
+    }
+}
+for(let i = 0; i < botonesMostrar.length; i++) {
+    let botonMostrar = botonesMostrar[i];
+    for(let j = 0; j < divdatos.length; j++) {
+        let divDato = divdatos[j];
+        if(botonMostrar.classList[1] == divDato.classList[1]){
+            botonMostrar.onclick = () => divDato.style.display = "";
+        }
+    }
+}
+
+
